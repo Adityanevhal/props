18ea6711dfc413072653a6e35d3288d0746252c6
Aditya Nevhal <adityanevhal1@gmail.com>
2026-02-22 19:18:09 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='parent'>
+      <div className="card">
+        <img src="image.png" alt="pic" />
+        <h1>Aditya Nevhal</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, iure?</p>
+         <button>View Profile</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
+

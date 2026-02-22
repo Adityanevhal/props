cf46687b836006c6f39441c633ad4ee8c9ecb2bf
Aditya Nevhal <adityanevhal1@gmail.com>
2026-02-22 23:31:19 +0530

    Replace static card with dynamic Card components

--- App.jsx
+++ App.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
+import Card from './components/card'
 
 const App = () => {
   return (
     <div className='parent'>
-      <div className="card">
-        <img src="image.png" alt="pic" />
-        <h1>Aditya Nevhal</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, iure?</p>
-         <button>View Profile</button>
-      </div>
+      <Card user="Pawani" age="20" img="https://i.pravatar.cc/150?img=1"/>
+      <Card user="Aditya" age="25" img="https://i.pravatar.cc/150?img=2"/>
+      <Card user="Arnav" age="22" img="https://i.pravatar.cc/150?img=3"/>
+   
     </div>
   )
 }
